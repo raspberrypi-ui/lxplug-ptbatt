a953b9d7ebcc16d93d2e02b2fd5b8a7a392ff8da
Simon Long <simon@raspberrypi.org>
2017-10-02 11:02:50 +0100

    First version - nowhere near complete...

---
+++ plugins/ptbatt/ptbatt.c
@@ -0,0 +1,408 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <errno.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib/gi18n.h>
+
+#include "plugin.h"
+
+#define ICON_BUTTON_TRIM 4
+
+//#define DEBUG_ON
+#ifdef DEBUG_ON
+#define DEBUG(fmt,args...) g_message("ptb: " fmt,##args)
+#else
+#define DEBUG
+#endif
+
+/* Plug-in global data */
+
+typedef struct {
+
+    GtkWidget *plugin;              /* Back pointer to the widget */
+    LXPanel *panel;                 /* Back pointer to panel */
+    GtkWidget *tray_icon;           /* Displayed image */
+    config_setting_t *settings;     /* Plugin settings */
+	guint global_timeout_ref;
+} PtBattPlugin;
+
+
+/* Prototypes */
+
+#define GRAY_LEVEL      0.93
+
+
+/* gdk_pixbuf_get_from_surface function from GDK+3 */
+
+static cairo_format_t
+gdk_cairo_format_for_content (cairo_content_t content)
+{
+  switch (content)
+    {
+    case CAIRO_CONTENT_COLOR:
+      return CAIRO_FORMAT_RGB24;
+    case CAIRO_CONTENT_ALPHA:
+      return CAIRO_FORMAT_A8;
+    case CAIRO_CONTENT_COLOR_ALPHA:
+    default:
+      return CAIRO_FORMAT_ARGB32;
+    }
+}
+
+static cairo_surface_t *
+gdk_cairo_surface_coerce_to_image (cairo_surface_t *surface,
+                                   cairo_content_t  content,
+                                   int              src_x,
+                                   int              src_y,
+                                   int              width,
+                                   int              height)
+{
+  cairo_surface_t *copy;
+  cairo_t *cr;
+
+  copy = cairo_image_surface_create (gdk_cairo_format_for_content (content),
+                                     width,
+                                     height);
+
+  cr = cairo_create (copy);
+  cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
+  cairo_set_source_surface (cr, surface, -src_x, -src_y);
+  cairo_paint (cr);
+  cairo_destroy (cr);
+
+  return copy;
+}
+
+static void
+convert_alpha (guchar *dest_data,
+               int     dest_stride,
+               guchar *src_data,
+               int     src_stride,
+               int     src_x,
+               int     src_y,
+               int     width,
+               int     height)
+{
+  int x, y;
+
+  src_data += src_stride * src_y + src_x * 4;
+
+  for (y = 0; y < height; y++) {
+    guint32 *src = (guint32 *) src_data;
+
+    for (x = 0; x < width; x++) {
+      guint alpha = src[x] >> 24;
+
+      if (alpha == 0)
+        {
+          dest_data[x * 4 + 0] = 0;
+          dest_data[x * 4 + 1] = 0;
+          dest_data[x * 4 + 2] = 0;
+        }
+      else
+        {
+          dest_data[x * 4 + 0] = (((src[x] & 0xff0000) >> 16) * 255 + alpha / 2) / alpha;
+          dest_data[x * 4 + 1] = (((src[x] & 0x00ff00) >>  8) * 255 + alpha / 2) / alpha;
+          dest_data[x * 4 + 2] = (((src[x] & 0x0000ff) >>  0) * 255 + alpha / 2) / alpha;
+        }
+      dest_data[x * 4 + 3] = alpha;
+    }
+
+    src_data += src_stride;
+    dest_data += dest_stride;
+  }
+}
+
+static void
+convert_no_alpha (guchar *dest_data,
+                  int     dest_stride,
+                  guchar *src_data,
+                  int     src_stride,
+                  int     src_x,
+                  int     src_y,
+                  int     width,
+                  int     height)
+{
+  int x, y;
+
+  src_data += src_stride * src_y + src_x * 4;
+
+  for (y = 0; y < height; y++) {
+    guint32 *src = (guint32 *) src_data;
+
+    for (x = 0; x < width; x++) {
+      dest_data[x * 3 + 0] = src[x] >> 16;
+      dest_data[x * 3 + 1] = src[x] >>  8;
+      dest_data[x * 3 + 2] = src[x];
+    }
+
+    src_data += src_stride;
+    dest_data += dest_stride;
+  }
+}
+
+/**
+ * gdk_pixbuf_get_from_surface:
+ * @surface: surface to copy from
+ * @src_x: Source X coordinate within @surface
+ * @src_y: Source Y coordinate within @surface
+ * @width: Width in pixels of region to get
+ * @height: Height in pixels of region to get
+ *
+ * Transfers image data from a #cairo_surface_t and converts it to an RGB(A)
+ * representation inside a #GdkPixbuf. This allows you to efficiently read
+ * individual pixels from cairo surfaces. For #GdkWindows, use
+ * gdk_pixbuf_get_from_window() instead.
+ *
+ * This function will create an RGB pixbuf with 8 bits per channel.
+ * The pixbuf will contain an alpha channel if the @surface contains one.
+ *
+ * Return value: (transfer full): A newly-created pixbuf with a reference
+ *     count of 1, or %NULL on error
+ */
+GdkPixbuf *
+gdk_pixbuf_get_from_surface  (cairo_surface_t *surface,
+                              gint             src_x,
+                              gint             src_y,
+                              gint             width,
+                              gint             height)
+{
+  cairo_content_t content;
+  GdkPixbuf *dest;
+
+  /* General sanity checks */
+  g_return_val_if_fail (surface != NULL, NULL);
+  g_return_val_if_fail (width > 0 && height > 0, NULL);
+
+  content = cairo_surface_get_content (surface) | CAIRO_CONTENT_COLOR;
+  dest = gdk_pixbuf_new (GDK_COLORSPACE_RGB,
+                         !!(content & CAIRO_CONTENT_ALPHA),
+                         8,
+                         width, height);
+
+  surface = gdk_cairo_surface_coerce_to_image (surface, content,
+                                               src_x, src_y,
+                                               width, height);
+  cairo_surface_flush (surface);
+  if (cairo_surface_status (surface) || dest == NULL)
+    {
+      cairo_surface_destroy (surface);
+      return NULL;
+    }
+
+  if (gdk_pixbuf_get_has_alpha (dest))
+    convert_alpha (gdk_pixbuf_get_pixels (dest),
+                   gdk_pixbuf_get_rowstride (dest),
+                   cairo_image_surface_get_data (surface),
+                   cairo_image_surface_get_stride (surface),
+                   0, 0,
+                   width, height);
+  else
+    convert_no_alpha (gdk_pixbuf_get_pixels (dest),
+                      gdk_pixbuf_get_rowstride (dest),
+                      cairo_image_surface_get_data (surface),
+                      cairo_image_surface_get_stride (surface),
+                      0, 0,
+                      width, height);
+
+  cairo_surface_destroy (surface);
+  return dest;
+}
+
+
+
+
+
+
+
+#define DISCHARGING 0
+#define CHARGING 1
+#define EXT_POWER 2
+
+int charge_level (int *status)
+{
+	static int charge = 100;
+	*status = DISCHARGING;
+	charge-=10;
+	return charge;
+}
+
+
+void update_icon (PtBattPlugin *pt)
+{
+	GdkPixbuf *pixbuf, *new_pixbuf;
+	cairo_t *cr;
+	cairo_format_t format;
+	cairo_surface_t *surface;
+	int capacity, status, width, height, w;
+	char str[255];
+	
+	// read the charge status
+	capacity = charge_level (&status);
+	
+	// load the raw icon from PNG file
+	pixbuf = gdk_pixbuf_new_from_file ("/usr/share/lxpanel/images/battery_icon.png", NULL);
+	
+	// get the drawing surface
+	format = (gdk_pixbuf_get_has_alpha (pixbuf)) ? CAIRO_FORMAT_ARGB32 : CAIRO_FORMAT_RGB24;
+	width = gdk_pixbuf_get_width (pixbuf);
+	height = gdk_pixbuf_get_height (pixbuf);
+	surface = cairo_image_surface_create (format, width, height);
+	
+	// draw base icon on surface
+	cr = cairo_create (surface);     
+	gdk_cairo_set_source_pixbuf (cr, pixbuf, 0, 0);
+	cairo_paint (cr);
+	
+	// fill the battery symbol
+	if (capacity < 0) w = 0;
+	else if (capacity > 100) w = 24;
+	else w = (99 * capacity) / 400;
+	
+	if (status == CHARGING)
+		cairo_set_source_rgb (cr, 1, 1, 0);
+	else if (capacity <= 20)
+		cairo_set_source_rgb (cr, 1, 0, 0);
+	else if (status == EXT_POWER)
+	    cairo_set_source_rgb (cr, 0.5, 0.5, 0.7);
+	else
+		cairo_set_source_rgb (cr, 0, 1, 0);
+	cairo_rectangle (cr, 5, 4, w, 12);
+	cairo_fill (cr);
+	
+	// empty the top end of the battery
+	if (w < 23)
+	{
+		cairo_set_source_rgb (cr, 1, 1, 1);
+		cairo_rectangle (cr, 5 + w, 4, 24 - w, 12);
+		cairo_fill (cr);
+	}
+	
+	// display the capacity figure
+	cairo_set_source_rgb (cr, GRAY_LEVEL, GRAY_LEVEL, GRAY_LEVEL);
+	cairo_rectangle (cr, 0, 20, 35, 15);
+	cairo_fill (cr);  
+	cairo_set_source_rgb (cr, 0, 0, 0);
+	cairo_select_font_face (cr, "Dosis", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+	cairo_set_font_size (cr, 12);
+	if (capacity >= 0) 
+	{
+		int x = 4;
+		if (capacity < 10) x += 4;
+		else if (capacity > 99) x -= 4;
+		cairo_move_to (cr, x, 33);
+		sprintf (str, "%2d%%", capacity);
+		cairo_show_text (cr, str);
+	}
+		
+	new_pixbuf = gdk_pixbuf_get_from_surface (surface, 0, 0, width, height);
+	g_object_ref_sink (pt->tray_icon);
+	gtk_image_set_from_pixbuf (GTK_IMAGE (pt->tray_icon), new_pixbuf);	
+	cairo_destroy (cr);
+	
+	// set the tooltip
+	gtk_widget_set_tooltip_text (pt->tray_icon, str);
+}
+
+static gboolean timer_event (PtBattPlugin *pt)
+{
+	g_source_remove(pt->global_timeout_ref);     // stop timer in case of tc_loop taking too long
+	update_icon (pt);
+	pt->global_timeout_ref = g_timeout_add (5000, (GSourceFunc) timer_event, (gpointer) pt);
+	return TRUE;
+}
+
+
+/* Plugin functions */
+
+static void ptbatt_popup_set_position (GtkMenu *menu, gint *px, gint *py, gboolean *push_in, gpointer data)
+{
+    PtBattPlugin *pt =  (PtBattPlugin *) data;
+    /* Determine the coordinates. */
+    lxpanel_plugin_popup_set_position_helper (pt->panel, pt->plugin, GTK_WIDGET(menu), px, py);
+    *push_in = TRUE;
+}
+
+/* Handler for menu button click */
+static gboolean ptbatt_button_press_event (GtkWidget *widget, GdkEventButton *event, LXPanel *panel)
+{
+    PtBattPlugin *pt = lxpanel_plugin_get_data (widget);
+
+#ifdef ENABLE_NLS
+    textdomain ( GETTEXT_PACKAGE );
+#endif
+	
+	return FALSE;
+}
+
+/* Handler for system config changed message from panel */
+static void ptbatt_configuration_changed (LXPanel *panel, GtkWidget *p)
+{
+    PtBattPlugin *pt = lxpanel_plugin_get_data (p);
+
+    //set_icon (panel, pt->tray_icon, "media-eject", 0);
+}
+
+/* Plugin destructor. */
+static void ptbatt_destructor (gpointer user_data)
+{
+    PtBattPlugin *pt = (PtBattPlugin *) user_data;
+
+    /* Deallocate memory */
+    g_free (pt);
+}
+
+/* Plugin constructor. */
+static GtkWidget *ptbatt_constructor (LXPanel *panel, config_setting_t *settings)
+{
+    /* Allocate and initialize plugin context */
+    PtBattPlugin *pt = g_new0 (PtBattPlugin, 1);
+    GtkWidget *p;
+    
+#ifdef ENABLE_NLS
+    setlocale (LC_ALL, "");
+    bindtextdomain ( GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR );
+    bind_textdomain_codeset ( GETTEXT_PACKAGE, "UTF-8" );
+    textdomain ( GETTEXT_PACKAGE );
+#endif
+
+    pt->tray_icon = gtk_image_new ();
+    //set_icon (panel, pt->tray_icon, "media-eject", 0);
+    gtk_widget_set_tooltip_text (pt->tray_icon, _("Select a drive in menu to eject safely"));
+    gtk_widget_set_visible (pt->tray_icon, TRUE);
+    update_icon (pt);
+
+    /* Allocate top level widget and set into Plugin widget pointer. */
+    pt->panel = panel;
+    pt->plugin = p = gtk_button_new ();
+    gtk_button_set_relief (GTK_BUTTON (pt->plugin), GTK_RELIEF_NONE);
+    g_signal_connect (pt->plugin, "button-press-event", G_CALLBACK(ptbatt_button_press_event), NULL);
+    pt->settings = settings;
+    lxpanel_plugin_set_data (p, pt, ptbatt_destructor);
+    gtk_widget_add_events (p, GDK_BUTTON_PRESS_MASK);
+
+    /* Allocate icon as a child of top level */
+    gtk_container_add (GTK_CONTAINER(p), pt->tray_icon);
+    
+    /* Show the widget, and return. */
+    gtk_widget_show_all (p);
+
+	pt->global_timeout_ref = g_timeout_add (5000, (GSourceFunc) timer_event, (gpointer) pt);
+
+    return p;
+}
+
+FM_DEFINE_MODULE(lxpanel_gtk, ptbatt)
+
+/* Plugin descriptor. */
+LXPanelPluginInit fm_module_init_lxpanel_gtk = {
+    .name = N_("PiTop Battery"),
+    .description = N_("Monitors PiTop battery"),
+    .new_instance = ptbatt_constructor,
+    .reconfigure = ptbatt_configuration_changed,
+    .button_press_event = ptbatt_button_press_event,
+    .gettext_package = GETTEXT_PACKAGE
+};
