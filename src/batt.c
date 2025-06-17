0b0d82b988b557f25fd92a449faa5b30a0d1421a
Simon Long <simon@raspberrypi.com>
2025-06-17 11:35:27 +0100

    Remove libfm dependency

--- src/batt.c
+++ src/batt.c
@@ -373,7 +373,7 @@ static GtkWidget *ptbatt_configure (LXPanel *panel, GtkWidget *plugin)
         conf_table);
 }
 
-int module_lxpanel_gtk_version = __FM_DEFINE_VERSION__(lxpanel_gtk);
+int module_lxpanel_gtk_version = 1;
 char module_name[] = PLUGIN_NAME;
 
 /* Plugin descriptor. */
